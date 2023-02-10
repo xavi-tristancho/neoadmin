cb35796c9e63ee88fc2598c959a9d9e6348757a9
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2023-02-10 18:07:18 +0100

    added first headers types

--- packages/backoffice/src/types.ts
+++ packages/backoffice/src/types.ts
@@ -6,4 +6,52 @@ export type Config = {
   router: BrowserRouterProps;
 };
 
-export type Header = { [key: string]: unknown };
+export type unknownObject = { [key: string]: unknown };
+
+export type Header = {
+  type: "CRUD" | "Page";
+  options: {
+    name: string;
+    primaryKey?: string;
+    requests?: {
+      findRequest: () => Promise<any[]>;
+      findOneRequest?: ({ id }: { id: string }) => Promise<any>;
+      countRequest?: ({
+        data: [],
+        fields: [],
+        filter: [],
+        pagination: {},
+        sort: [],
+      }) => Promise<number>;
+      upsertRequest(data: unknownObject): Promise<any[]>;
+      deleteRequest?: ({ id }: { id: string }) => Promise<void>;
+      uploadFileRequest?: (file: File) => Promise<string>;
+      mapFindResponse?: (response: any[]) => any[];
+    };
+    route: {
+      path: string;
+      home?: boolean;
+      unAuth?: boolean;
+      auth?: boolean;
+      exact?: boolean;
+      component?: (props?: any) => Promise<JSX.Element>;
+    };
+    tableOptions?: {
+      children?: (state: unknownObject) => JSX.Element;
+      component?: (
+        props: any,
+        ModelTable: React.ComponentType<any>
+      ) => React.ReactElement;
+    };
+    getItemActions?: (
+      item: unknownObject,
+      state: unknownObject
+    ) => { isEditable: boolean; isDeletable: boolean };
+    isCreatable?: boolean;
+    isEditable?: boolean;
+    isDeletable?: boolean;
+    openOnClickRow?: boolean;
+    pageSize?: number;
+    renderBefore?: () => React.ReactNode;
+  };
+};
