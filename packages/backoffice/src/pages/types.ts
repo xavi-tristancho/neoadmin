d8c7c3c414c2426e8b47c3685dc6c8902fad1824
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2023-02-17 13:14:44 +0100

    fixed error type in UnAuthPageForm fields

--- packages/backoffice/src/pages/types.ts
+++ packages/backoffice/src/pages/types.ts
@@ -2,10 +2,11 @@ import { Credentials } from "@neoco/neoco-backoffice/src/types";
 import { Field } from "@neoco/neoco-form/src/types";
 import { LoginInputProps } from "../contexts/AuthContext";
 
-export type UnAuthPageFormProps = UnAuthFormProps & {
+export type UnAuthPageFormProps = {
   onSubmit: (credentials: Credentials) => Promise<LoginInputProps>;
   afterSubmit?: (args: LoginInputProps) => void;
   page?: string;
+  fields?: unknownObject[];
   title?: string;
   submitText?: string;
   fields?: Field[];
