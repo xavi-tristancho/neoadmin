b085dcc5d71d3fd88a2784b968ad145571e65a93
Xavier Tristancho Bordoy <xavi.tristancho@neoco.dev>
2023-02-22 17:31:17 +0100

    Added Github pages base url

--- apps/demo/src/index.tsx
+++ apps/demo/src/index.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import { apps, contexts } from "@neoco/neoco-backoffice";
+import reportWebVitals from "./reportWebVitals";
 import { headers } from "./pages";
 import "@neoco/neoco-backoffice/dist/styles.css";
 
 const { App } = apps;
 const { AuthProvider, ConfigProvider } = contexts;
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <ConfigProvider>
-        <App headers={headers} />
+        <App
+          headers={headers}
+          config={{ router: { basename: "neoadmin-demo" } }}
+        />
       </ConfigProvider>
     </AuthProvider>
   </React.StrictMode>
--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "neoadmin-demo",
   build: {
     outDir: "./build",
   },
