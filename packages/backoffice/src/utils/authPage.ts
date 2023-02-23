83e3cdb7283f1d3108ad07ab9db1fb1e4d57e9b0
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2023-02-23 09:15:31 +0100

    added types to authPage

--- packages/backoffice/src/utils/authPage.ts
+++ packages/backoffice/src/utils/authPage.ts
@@ -1,4 +1,4 @@
-const getModelInitialLetter = (name = "") =>
+const getModelInitialLetter = (name: string): string =>
   name === "" ? "" : name.split("/")?.[1]?.[0]?.toUpperCase();
 
 export default getModelInitialLetter;
