27bdb67f68f57af915f5f6af15c0e7a297c0e0eb
Xavier Tristancho Bordoy <xavi.tristancho@neoco.dev>
2023-02-23 14:57:36 +0100

    Added slashes on basename

--- apps/demo/src/index.tsx
+++ apps/demo/src/index.tsx
@@ -3,23 +3,19 @@ import { createRoot } from "react-dom/client";
 import { apps, contexts } from "@neoco/neoco-backoffice";
 import reportWebVitals from "./reportWebVitals";
 import { headers } from "./pages";
-import "@neoco/neoco-backoffice/dist/styles.css";
-
-const { App } = apps;
-const { AuthProvider, ConfigProvider } = contexts;
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <ConfigProvider>
-        <App
+    <contexts.AuthProvider>
+      <contexts.ConfigProvider>
+        <apps.App
           headers={headers}
-          config={{ router: { basename: "neoadmin-demo" } }}
+          config={{ router: { basename: "/neoadmin-demo" } }}
         />
-      </ConfigProvider>
-    </AuthProvider>
+      </contexts.ConfigProvider>
+    </contexts.AuthProvider>
   </React.StrictMode>
 );
 
--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,18 +1,12 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "neoadmin-demo",
+  base: "/neoadmin-demo",
   build: {
     outDir: "./build",
-  },
-  resolve: {
-    alias: {
-      "@neoco/backoffice": path.resolve(__dirname, "../neoco-backoffice/"),
-    },
   },
   plugins: [
     react(),
