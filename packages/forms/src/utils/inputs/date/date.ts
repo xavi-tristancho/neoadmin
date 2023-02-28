7c2a89a599c70de9f57b3163f86fb82a9c126f32
ma.navarro <ma.navarro@neoco.dev>
2023-02-28 13:30:06 +0100

    Revert "finished"
    This reverts commit b07a6efb4437c61a1b031ffb16f9df13362c9b3c.

--- packages/forms/src/utils/inputs/date/date.ts
+++ packages/forms/src/utils/inputs/date/date.ts
@@ -34,7 +34,7 @@ const date = ({
 }: DateInput): DateOutput => {
   const formatValue: string = format({ state, field });
   const parsedDate = DateTime.fromISO(formatValue);
-  const value: string = parsedDate.isValid
+  const value = parsedDate.isValid
     ? parsedDate.toFormat(toFormat)
     : formatValue;
 
