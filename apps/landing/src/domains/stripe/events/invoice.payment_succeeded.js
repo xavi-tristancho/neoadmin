7010ba8ef15ebb6c379401f2aba582d541a9008c
Xavier Tristancho Bordoy <xavi.tristancho@neoco.dev>
2023-03-15 17:14:56 +0100

    Added mailgun email send

--- apps/landing/src/domains/stripe/events/invoice.payment_succeeded.js
+++ apps/landing/src/domains/stripe/events/invoice.payment_succeeded.js
@@ -4,10 +4,19 @@ import {
   AddUserToGroupCommand,
   CreateAccessKeyCommand,
 } from "@aws-sdk/client-iam";
+import Mailgun from "mailgun.js";
+import formData from "form-data";
 import { INCORRECT_PRODUCT } from "../errors";
 
 const NEOADMIN_IAM_GROUP = "neoAdmin";
 const client = new IAMClient({ region: "eu-west-1" });
+
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_API_KEY,
+  url: "https://api.eu.mailgun.net",
+});
 
 const isNeoAdminProduct = (line) =>
   line.plan.product === process.env.STRIPE_NEOADMIN_PRODUCT;
@@ -34,9 +43,22 @@ export async function handler(event) {
       const { AccessKey } = await client.send(createAccessKeyCommand);
       const { AccessKeyId, SecretAccessKey } = AccessKey;
 
-      // Send email
+      const data = {
+        from: "Mailgun Sandbox <postmaster@mailgun.neoadmin.neoco.dev>",
+        to: object.customer_email,
+        subject: "Thank you for your purchase!",
+        template: "neoadmin-purchase",
+        "h:X-Mailgun-Variables": JSON.stringify({
+          firstname: object.customer_name,
+          access_key: AccessKeyId,
+          secret_key: SecretAccessKey,
+        }),
+      };
+
+      await mg.messages.create(process.env.MAILGUN_DOMAIN, data);
     } catch (error) {
       console.error({ error });
+      return { message: "Event not handled", error };
     }
 
     return { message: "Event handled" };
