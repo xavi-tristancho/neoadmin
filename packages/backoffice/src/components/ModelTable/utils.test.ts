ed1020148a4595b441a423b78b0aa1470197b5d4
Xavier Tristancho Bordoy <xavi.tristancho@neoco.dev>
2023-03-15 12:36:41 +0100

    Fixed test

--- packages/backoffice/src/components/ModelTable/utils.test.ts
+++ packages/backoffice/src/components/ModelTable/utils.test.ts
@@ -31,164 +31,168 @@ const header: Header = {
   ],
 };
 
-describe("regarding the getFields function", () => {
-  it("should be able to get all the fields of all the sections of the header", () => {
-    expect(getFields({ header, t: (text) => text })).toEqual([
-      {
-        field: "concept",
-        flex: 1,
-        headerName: "pages.undefined.model.concept",
-        name: "pages.undefined.model.concept",
-        property: "concept",
-      },
-      {
-        field: "hours",
-        flex: 1,
-        headerName: "pages.undefined.model.hours",
-        name: "pages.undefined.model.hours",
-        property: "hours",
-      },
-    ]);
-  });
-});
-
-describe("regarding the getItemIdentifier function", () => {
-  it("should return id field if it exists", () => {
-    expect(getItemIdentifier({ id: 1 })).toEqual(1);
-  });
-
-  it("should be able to get identifier for item based on header config", () => {
-    expect(getItemIdentifier({ concept: "sample" }, header)).toEqual("sample");
-  });
-
-  it("cannot get identifier if header config not permit and item not hace id property", () => {
-    expect(getItemIdentifier({ hours: "sample" }, header)).toBeUndefined();
-  });
-});
-
-describe("regarding the getFilterFields function", () => {
-  describe("given a header with the fields' sections without filters", () => {
-    it("should return an empty array", () => {
-      const emptyHeader: Header = {
-        options: {
-          name: "Test",
-          primaryKey: "concept",
-          route: { path: "/test", home: true },
+describe("this", () => {
+  describe("regarding the getFields function", () => {
+    it("should be able to get all the fields of all the sections of the header", () => {
+      expect(getFields({ header, t: (text) => text })).toEqual([
+        {
+          field: "concept",
+          flex: 1,
+          headerName: "pages.undefined.model.concept",
+          name: "pages.undefined.model.concept",
+          property: "concept",
         },
-        type: "CRUD",
-        sections: [
-          {
-            title: "Principal",
-            fields: [
-              {
-                type: "text",
-                property: "customerId",
-              },
-              {
-                type: "text",
-                property: "name",
-              },
-              {
-                type: "text",
-                property: "city",
-                tableOptions: {},
-              },
-              {
-                type: "text",
-                property: "postalCode",
-              },
-              {
-                type: "text",
-                property: "phone",
-                tableOptions: {},
-              },
-            ],
-          },
-        ],
-      };
-      expect(getFilterFields(emptyHeader)).toEqual([]);
+        {
+          field: "hours",
+          flex: 1,
+          headerName: "pages.undefined.model.hours",
+          name: "pages.undefined.model.hours",
+          property: "hours",
+        },
+      ]);
     });
   });
 
-  describe("given a header with the fields sections with filters", () => {
-    it("should return an array with the fields that contain filters", () => {
-      const filtersHeader: Header = {
-        options: {
-          name: "Test",
-          primaryKey: "concept",
-          route: { path: "/test", home: true },
-        },
-        type: "CRUD",
-        sections: [
+  describe("regarding the getItemIdentifier function", () => {
+    it("should return id field if it exists", () => {
+      expect(getItemIdentifier({ id: 1 })).toEqual(1);
+    });
+
+    it("should be able to get identifier for item based on header config", () => {
+      expect(getItemIdentifier({ concept: "sample" }, header)).toEqual(
+        "sample"
+      );
+    });
+
+    it("cannot get identifier if header config not permit and item not hace id property", () => {
+      expect(getItemIdentifier({ hours: "sample" }, header)).toBeUndefined();
+    });
+  });
+
+  describe("regarding the getFilterFields function", () => {
+    describe("given a header with the fields' sections without filters", () => {
+      it("should return an empty array", () => {
+        const emptyHeader: Header = {
+          options: {
+            name: "Test",
+            primaryKey: "concept",
+            route: { path: "/test", home: true },
+          },
+          type: "CRUD",
+          sections: [
+            {
+              title: "Principal",
+              fields: [
+                {
+                  type: "text",
+                  property: "customerId",
+                },
+                {
+                  type: "text",
+                  property: "name",
+                },
+                {
+                  type: "text",
+                  property: "city",
+                  tableOptions: {},
+                },
+                {
+                  type: "text",
+                  property: "postalCode",
+                },
+                {
+                  type: "text",
+                  property: "phone",
+                  tableOptions: {},
+                },
+              ],
+            },
+          ],
+        };
+        expect(getFilterFields(emptyHeader)).toEqual([]);
+      });
+    });
+
+    describe("given a header with the fields sections with filters", () => {
+      it("should return an array with the fields that contain filters", () => {
+        const filtersHeader: Header = {
+          options: {
+            name: "Test",
+            primaryKey: "concept",
+            route: { path: "/test", home: true },
+          },
+          type: "CRUD",
+          sections: [
+            {
+              title: "Principal",
+              fields: [
+                {
+                  type: "multiselect",
+                  property: "customerId",
+                  relation: {
+                    isMulti: true,
+                  },
+                },
+                {
+                  type: "multiselect",
+                  property: "name",
+                  relation: {
+                    isMulti: true,
+                  },
+                },
+                {
+                  type: "text",
+                  property: "city",
+                  tableOptions: {
+                    filter: (item: unknown) => String(item),
+                  },
+                },
+                {
+                  type: "text",
+                  property: "postalCode",
+                },
+                {
+                  type: "text",
+                  property: "phone",
+                  tableOptions: {
+                    isSearchable: true,
+                  },
+                },
+              ],
+            },
+          ],
+        };
+        expect(getFilterFields(filtersHeader)).toEqual([
           {
-            title: "Principal",
-            fields: [
-              {
-                type: "multiselect",
-                property: "customerId",
-                relation: {
-                  isMulti: true,
-                },
-              },
-              {
-                type: "multiselect",
-                property: "name",
-                relation: {
-                  isMulti: true,
-                },
-              },
-              {
-                type: "text",
-                property: "city",
-                tableOptions: {
-                  filter: (item: unknown) => String(item),
-                },
-              },
-              {
-                type: "text",
-                property: "postalCode",
-              },
-              {
-                type: "text",
-                property: "phone",
-                tableOptions: {
-                  isSearchable: true,
-                },
-              },
-            ],
+            type: "multiselect",
+            property: "customerId",
+            relation: {
+              isMulti: true,
+            },
           },
-        ],
-      };
-      expect(getFilterFields(filtersHeader)).toEqual([
-        {
-          type: "multiselect",
-          property: "customerId",
-          relation: {
-            isMulti: true,
+          {
+            type: "multiselect",
+            property: "name",
+            relation: {
+              isMulti: true,
+            },
           },
-        },
-        {
-          type: "multiselect",
-          property: "name",
-          relation: {
-            isMulti: true,
+          {
+            type: "text",
+            property: "city",
+            tableOptions: {
+              filter: expect.any(Function) as () => string,
+            },
           },
-        },
-        {
-          type: "text",
-          property: "city",
-          tableOptions: {
-            filter: (item: unknown) => String(item),
+          {
+            type: "text",
+            property: "phone",
+            tableOptions: {
+              isSearchable: true,
+            },
           },
-        },
-        {
-          type: "text",
-          property: "phone",
-          tableOptions: {
-            isSearchable: true,
-          },
-        },
-      ]);
+        ]);
+      });
     });
   });
 });
