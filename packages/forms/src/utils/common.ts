36d0a745c1dacd83b8722908a56e27fba44b767f
ma.navarro <ma.navarro@neoco.dev>
2023-03-15 10:29:06 +0100

    test conflict resolved

--- packages/forms/src/utils/common.ts
+++ packages/forms/src/utils/common.ts
@@ -1,15 +1,16 @@
 import { unknownObject } from "@neoco/neoco-backoffice/src/types";
 import { Field } from "../types";
 
-export const isFunction = (prop: unknown): boolean =>
-  typeof prop === "function";
+type RemoveIfNotVisible = (item?: unknownObject) => (field?: Field) => boolean;
 
-export const removeIfNotVisible =
-  (item?: unknownObject) =>
-  (field?: Field): boolean => {
+export const removeIfNotVisible: RemoveIfNotVisible = (item) => (field) => {
+  if (field) {
     const { upsertOptions: { show } = {} } = field;
     const isUndefined: boolean = typeof show === "undefined";
     const value: boolean = typeof show === "function" ? show(item) : show;
 
     return isUndefined ? true : value;
-  };
+  } else {
+    return true;
+  }
+};
