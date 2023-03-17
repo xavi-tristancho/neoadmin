6f8616fec999da5e2d7abc5ebcd26f39d9afc530
Victor Garcia-Vao <vct2295@gmail.com>
2023-03-17 12:36:29 +0100

    added getBase64FromUrl test

--- packages/image-uploader/src/utils/file.test.ts
+++ packages/image-uploader/src/utils/file.test.ts
@@ -1,11 +1,20 @@
 import { describe, it, expect } from "vitest";
-import { blobToBase64 } from "./file";
+import { blobToBase64, getBase64FromUrl } from "./file";
 
 describe("Regarding blobToBase64 function", () => {
   describe("when a valid blob is passed", () => {
     it("should return a promise", () => {
       const blob = new Blob(["Hello, world!"], { type: "text/plain" });
       const result = blobToBase64(blob);
+      expect(result).toBeInstanceOf(Promise);
+    });
+  });
+});
+
+describe("Regarding getBase64FromUrl function", () => {
+  describe("when a valid url is passed", () => {
+    it("should return a promise", () => {
+      const result = getBase64FromUrl("https://example.com");
       expect(result).toBeInstanceOf(Promise);
     });
   });
